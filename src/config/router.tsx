7c16df83870bf1e870cad83d9c0ba72a7afe60eb
tanahiro2010 <herentongkegu087@gmail.com>
2026-07-02 19:31:52 +0900

    feat: 実装済みページのルートを追加

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -1,10 +1,23 @@
 import type { AppRoute } from "../types/router"
+import App from "../app/Home"
+import AuthCallback from "../app/AuthCallback"
+import Login from "../app/Login"
 
 const routes: AppRoute[] = [
   {
-    type: "layout",
-    element: <></>, // ここに
-    children: []
+    path: "/",
+    type: "page",
+    element: <App />,
+  },
+  {
+    path: "/_auth/callback",
+    type: "page",
+    element: <AuthCallback />,
+  },
+  {
+    path: "/_auth/login",
+    type: "page",
+    element: <Login />,
   }
 ]
 
